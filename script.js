75dd0726b9215bfd764c60f52236887de4f6c600
fanasina73-design <fanasinahenintsoa73@gmail.com>
2025-10-20 13:23:42 +0300

    Midification de site

---
+++ script.js
@@ -0,0 +1,15 @@
+// Sélection du conteneur où on va ajouter le bouton
+const menu = document.getElementById("menu");
+
+// Création du bouton Home
+const btnHome = document.createElement("button");
+btnHome.textContent = "Home";
+
+// Ajout du bouton dans le menu
+menu.appendChild(btnHome);
+
+// Événement : quand on clique sur "Home", aller à la section correspondante
+btnHome.addEventListener("click", () => {
+  const homeSection = document.getElementById("home");
+  homeSection.scrollIntoView({ behavior: "smooth" });
+});
